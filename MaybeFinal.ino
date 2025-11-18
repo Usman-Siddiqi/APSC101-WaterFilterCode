790e6dee20690c540d6fadb39931edc30801a097
CodeWizReal <timothyvandenbroek1@gmail.com>
2025-11-18 10:23:10 -0500

    Update MaybeFinal.ino

--- MaybeFinal.ino
+++ MaybeFinal.ino
@@ -1,48 +1,132 @@
-/*
- * APSC 101 Example Sketch
- * 
- * B. Frank, adapted from R. Smyk for APSC 101, 2024.07.16
- * 
- * Simple example code for testing both Turbidity Sensors. 
- * Original code from DFRobot: https://wiki.dfrobot.com/Turbidity_sensor_SKU__SEN0189
- * 
- * !!! Must have the turbidity sensors connected on pin A0 and A1 as demonstrated in Arduino Instructions. !!!
- * !!! If you have turbidity sensors connected at different pins, you must replace those in code !!!
- * 
- */
-unsigned long currentMillis=0; // Stores the the current time
-unsigned long previousMillis = 0; // Stores the last time the serial monitor was updated
-const unsigned long interval = 1000;
-int seconds = 0; // Counter for seconds
+#include <AFMotor.h>
+
+// Define the pins the push buttons and limit switch are connected to
+#define emergencyStopButton A2
+#define startButton A3
+#define startTurbiditySensor A0
+#define endTurbiditySensor A1
+#define ppump 53//Moves clean to clean tank
+
+// Connect the DC motor to M1 on the motor control board
+AF_DCMotor DCmotor1(2);//Runs threaded rod
+AF_DCMotor DCmotor2(1);//Runs impeller
+AF_DCMotor pump(4);//Moves dirty to coagulation
+AF_DCMotor cfpump(3);//slurry to coagulation tank
+
+void dirty_to_coagulation(){
+  cfpump.setSpeed(0);
+  pump.setSpeed(255);//Moving the water for this task
+  DCmotor1.setSpeed(0);
+  DCmotor2.setSpeed(0);
+  digitalWrite(ppump, LOW);
+}
+
+void slurry_to_coagulation(){
+  cfpump.setSpeed(255);//Moving the water for this task
+  pump.setSpeed(0);
+  DCmotor1.setSpeed(0);
+  DCmotor2.setSpeed(0);
+  digitalWrite(ppump, LOW);
+}
+
+void coagulation_to_clean(){
+  cfpump.setSpeed(0);
+  pump.setSpeed(0);
+  DCmotor1.setSpeed(0);
+  DCmotor2.setSpeed(0);
+  digitalWrite(ppump, HIGH);//Moving the water for this task
+}
+
+bool coagulation(){
+  cfpump.setSpeed(0);
+  pump.setSpeed(0);
+  DCmotor1.setSpeed(167);//Runs the mixing bar
+  DCmotor2.setSpeed(167);//Runs impeller
+  digitalWrite(ppump, LOW);
+
+  bool emergency = false;
+  int mixing_loops = 1;
+  for(int i = 0; i < mixing_loops; i++){
+      DCmotor1.run(FORWARD);
+      emergency = wait(100);//1 second delay
+      if(emergency)
+        return true;
+      DCmotor1.run(BACKWARD);
+      emergency = wait(100);//1 second delay
+      if(emergency)
+        return true;
+  }
+  return false;
+}
+
+bool wait(int time){//time is in 10 miliseconds. This waits and returns ture if emergency stop button is pressed
+  for(int i = 0; i < time; i++){
+    bool emergencyStopButtonState = digitalRead(emergencyStopButton);
+    if(emergencyStopButtonState)
+      return true;
+    
+    delay(10);
+  }
+  return false;
+}
+
+void stop_all(){
+  cfpump.setSpeed(0);
+  pump.setSpeed(0);
+  DCmotor1.setSpeed(0);
+  DCmotor2.setSpeed(0);
+  digitalWrite(ppump, LOW);
+}
+
+float read_turbidity(int a_pin){
+  int sensor = analogRead(a_pin);// read the input on analog pin 0
+  float voltage = sensor * (5.0 / 1024.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V)
+  Serial.println("Voltage: ");
+  Serial.println(voltage); 
+}
 
 void setup() {
-  Serial.begin(9600);  // Opens serial port and sets rate to 9600 bits per sec. This has to match number on serial monitor!!
- const unsigned long interval = 1000;
-  unsigned long previousMillis = 0; // Stores the last time the serial monitor was updated
-  int seconds = 0; // Counter for seconds
-  Serial.print("Time\tSensor 1\tSensor2\n");
-  Serial.print("====\t========\t=======\n");  
+  // Set up serial communication to print things to the serial monitors
+  Serial.begin (9600);
+  pinMode(emergencyStopButton, INPUT_PULLUP);
+  pinMode(startButton, INPUT_PULLUP);
+  pinMode(ppump, OUTPUT);
+
+  pump.run(FORWARD);
+  cfpump.run(FORWARD);
+  DCmotor1.run(FORWARD);
+  DCmotor2.run(FORWARD);
+
+  // Set the speed of the Pumps. The speed input can be 0-255.
+  cfpump.setSpeed(0);
+  pump.setSpeed(0);
+  DCmotor1.setSpeed(0);
+  DCmotor2.setSpeed(0);
+  digitalWrite(ppump, LOW);
 }
+
 void loop() {
-   // Get the current time in milliseconds
-  currentMillis = millis();
-  
-  // Check if the interval has passed
-  if (currentMillis - previousMillis >= interval) {
-    // Save the last time you updated the serial monitor
-    previousMillis = currentMillis;
-    
-    // Increment the seconds counter
-    seconds++;
-  
-  int sensor1 = analogRead(A0);// read the input on analog pin 0
-  float voltage1 = sensor1 * (5.0 / 1024.0); // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V)
-  int sensor2 = analogRead(A1);
-  float voltage2 = sensor2 * (5.0 / 1024.0); 
-  Serial.print(seconds);
-  Serial.print("\t");
-  Serial.print(voltage1); 
-  Serial.print("\t\t");
-  Serial.println(voltage2); // println means next loop will print on new line
+  bool startButtonState = digitalRead(startButton);
+  while(startButtonState){
+    bool emergency = false;
+    float initial_turbidity = read_turbidity(startTurbiditySensor);
+    dirty_to_coagulation();
+    if(wait(3000))//30 second delay
+      break;
+    slurry_to_coagulation();
+    if(wait(3000))//30 second delay
+      break;
+    emergency = coagulation();
+    if(emergency)
+      break;
+    if(wait(3000))//30 second delay
+      break;
+    coagulation_to_clean();
+    if(wait(10))//30 second delay
+      break;
+    float final_turbidity = read_turbidity(endTurbiditySensor);
+    Serial.print("Voltage: ");
+    Serial.println(initial_turbidity - final_turbidity);
   }
+  stop_all();
 }
